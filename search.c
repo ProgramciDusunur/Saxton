ddef7dd4917683c3d53bf6fe30402efbe292a86e
Eren Araz <lerenarazl@gmail.com>
2025-11-15 23:45:26 +0300

    fix implementation and try again bench: 1077769

--- search.c
+++ search.c
@@ -332,17 +332,18 @@ void search_root(GameState *pos, SearchInfo *root_info)
     root_info->ply = 0;
     root_info->stopped = false;
     memset(root_info->move_stack, 0, sizeof(root_info->move_stack));
+    memset(root_info->pv_table, 0, sizeof(root_info->pv_table));
+    memset(root_info->pv_table_length, 0, sizeof(root_info->pv_table_length));
 
     for (int iterative_depth = 1; iterative_depth <= max_search_depth; iterative_depth++) {
-        memset(root_info->pv_table, 0, sizeof(root_info->pv_table));
-        memset(root_info->pv_table_length, 0, sizeof(root_info->pv_table_length));
+        
         root_info->depth = iterative_depth;
 
-        int new_score = search(alpha, beta, iterative_depth, pos, root_info);
-
-        // If time is up, and we have completed at least depth 1 search, break out of loop
-        if (!root_info->pv_table_length[0] || (root_info->stopped == 1 && iterative_depth > 1))
+        if (!root_info->pv_table_length[0] && root_info->stopped == 1)
             break;
+
+        int new_score = search(alpha, beta, iterative_depth, pos, root_info);        
+        
 
         // Basic Aspiration Windows
         if (new_score <= alpha || new_score >= beta) {
